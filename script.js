795a4aeeac5254904a881732262726cab4b0c4f7
Ali Abu Salam <aliadadehmav@gmail.com>
2023-06-08 21:58:22 +0700

    Add script to implement changing rating by clicking rating buttons

---
+++ script.js
@@ -0,0 +1,23 @@
+let rating = null;
+
+const handleRatingClick =(ratingButtons) => (event) => {
+  event.preventDefault();
+  activeButton = ratingButtons.find(button => button.classList.contains('on'));
+
+  if(activeButton){ //turn off any previous active button before activating the clicked button
+    activeButton.classList.remove('on');
+    activeButton.classList.add('off');
+  }
+  const target = event.target
+  rating = target.textContent;
+  console.log('rating: ', rating);
+  target.classList.remove('off')
+  target.classList.add('on');
+}
+
+let ratingButtonChildren = document.getElementById('rating-button').childNodes;
+let ratingButtons = Array.from(ratingButtonChildren).filter(child => child.nodeName === 'DIV');
+
+ratingButtons.forEach(button => button.addEventListener('click', handleRatingClick(ratingButtons)));
+
+console.log('element: ', ratingButtons);
